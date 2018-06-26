617fae2095f10026605d43fd23944681028a6b5b
auchong <auchong@deloitte.com>
2018-06-26 16:09:23 -0400

    add routes for various pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -92,29 +92,16 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    this.setState({loading: true});
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Austin Chong',
-        address: {
-          street: 'Test St',
-          zipCode: '123456',
-          country: 'USA'
-        },
-        email: 'test@test.com',
-        deliveryMethod: 'fastest'
-      }
-    }
-
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({loading: false, purchasing: false})
-      })
-      .catch(error => {
-        this.setState({loading: false, purchasing: false})
-      })
+    const queryParmas = [];
+    for (let i in this.state.ingredients) {
+      queryParmas.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+    };
+    queryParmas.push('price=' + this.state.totalPrice);
+    const queryString = queryParmas.join('&');
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    });
   }
 
   render() {
